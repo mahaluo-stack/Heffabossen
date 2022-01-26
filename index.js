0ac14f9f67e66a80fe359bca8f4c848f1539deb3
citissime <maxhaggqvist@gmail.com>
2022-01-26 14:58:21 +1100

    procfile

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+
+const dotenv = require('dotenv');
+const { Client, Intents, MessageEmbed } = require('discord.js');
+
+dotenv.config();
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
+
 const WOODHOUSE = process.env.WOODHOUSE;
 const TEST_CHANNEL = process.env.TEST_CHANNEL;
-
-const { Client, Intents, MessageEmbed } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
 
 client.on('ready', () => {
     console.log('ready');
