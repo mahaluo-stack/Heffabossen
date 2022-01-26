8437783d92865f1ea09aff7aaf2c36cc18655583
citissime <maxhaggqvist@gmail.com>
2022-01-26 19:17:05 +1100

    procfile

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 
-const dotenv = require('dotenv');
-dotenv.config();
-
 const { Client, Intents, MessageEmbed } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
 
