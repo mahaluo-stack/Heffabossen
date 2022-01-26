575eb1631f1a0e8485d287b79ebcb4c89ef99eb2
citissime <maxhaggqvist@gmail.com>
2022-01-26 14:45:57 +1100

    procfile

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 const WOODHOUSE = process.env.WOODHOUSE;
 const TEST_CHANNEL = process.env.TEST_CHANNEL;
-const Discord = require('discord.js');
-const client = new Discord.Client();
+
+const { Client, Intents, MessageEmbed } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
 
 client.on('ready', () => {
     console.log('ready');
